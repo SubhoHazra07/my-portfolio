27013f9d4441324dab851545b61a4cba5c54ba4a
Subho Hazra <159612743+SubhoHazra07@users.noreply.github.com>
2025-03-19 23:08:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  assetPrefix: "./",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
